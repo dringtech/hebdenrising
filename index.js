fe966ce0a3b6479545a110eabf6be80af6811b19
gilesdring <giles@dringtech.com>
2015-12-02 00:29:29 +0000

    Adding index.js - AWS Lambda invocation

---
+++ index.js
@@ -0,0 +1,17 @@
+var scrapea = require('./scrapea');
+var q = require('q');
+
+exports.handler = function(event, context) {
+  // console.log('value1 =', event.key1);
+  // console.log('value2 =', event.key2);
+  // console.log('value3 =', event.key3);
+  q.all(
+    ['8205', '8343', '8097', '8048', 8152].map(scrapea.getStationData)
+  )
+  .then(function(results) {
+    context.succeed(results);  // Echo back the first key value
+  })
+  .fail(function(error) {
+    context.fail(error);
+  });
+};
