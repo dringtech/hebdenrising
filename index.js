53970ce110544144aab99ead98fa561fa07c581d
Giles Dring <gilesdring@gmail.com>
2015-12-03 22:24:47 +0000

    Making the lambda function respond to json event parameters

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var scrapea = require('./scrapea');
-var q = require('q');
 
 exports.handler = function(event, context) {
-  console.log(JSON.stringify(event, null, 2));
+  console.log('Station ID = ', event.stationId);
 
-  // console.log('value1 =', event.key1);
-  // console.log('value2 =', event.key2);
-  // console.log('value3 =', event.key3);
-  q.all(
-    ['8205', '8343', '8097', '8048', 8152].map(scrapea.getStationData)
-  )
+  scrapea.getStationData(event.stationId)
   .then(function(results) {
-    context.succeed(results);  // Echo back the first key value
+    context.succeed(results);
   })
   .fail(function(error) {
     context.fail(error);
